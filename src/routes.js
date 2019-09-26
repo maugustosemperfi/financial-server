a7f615361372ca3a5aa076a52f0d72fe4a70236c
Marcos Augusto <augustoprofemp@gmail.com>
2019-09-25 21:28:01 -0300

    [UPDATE] Updating accounts overview route

--- src/routes.js
+++ src/routes.js
@@ -10,9 +10,9 @@ routes.post('/user', UserController.store);
 routes.post('/login', UserController.login);
 
 routes.use(authMiddleware);
-routes.get('/overview', AccountController.overview);
 routes.post('/accounts', AccountController.store);
 routes.get('/accounts', AccountController.getAll);
+routes.get('/accounts/overview', AccountController.overview);
 
 routes.post('/records', RecordController.store);
 
