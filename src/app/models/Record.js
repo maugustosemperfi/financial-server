e78a75ac334143aeda4d79f996184cab15706a27
Marcos Augusto <augustoprofemp@gmail.com>
2019-11-13 20:40:02 -0300

    [FEATURE] Grouping records by record day
    - Creating virtual column on records (recordDay)
    - Creating function to group records by day

--- src/app/models/Record.js
+++ src/app/models/Record.js
@@ -1,13 +1,26 @@
 import Sequelize, { Model } from 'sequelize';
+import { format } from 'date-fns';
 
 class Record extends Model {
   static init(sequelize) {
     super.init(
       {
         description: Sequelize.STRING,
-        value: Sequelize.DECIMAL,
+        value: {
+          type: Sequelize.DECIMAL,
+          get() {
+            const value = this.getDataValue('value');
+            return value === null ? null : Number(value);
+          },
+        },
         type: Sequelize.INTEGER,
         recordDate: Sequelize.DATE,
+        recordDay: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return format(this.recordDate, 'yyyy-MM-dd');
+          },
+        },
       },
       {
         sequelize,
--- src/app/services/RecordService.js
+++ src/app/services/RecordService.js
@@ -3,19 +3,71 @@ import { Op } from 'sequelize';
 import Account from '../models/Account';
 import CreditCard from '../models/CreditCard';
 import Record from '../models/Record';
+import Bank from '../models/Bank';
 
 class RecordService {
   async getTransactionsRecords(userId, date) {
     const records = await Record.findAll({
       where: { recordDate: { [Op.between]: [startOfDay(startOfMonth(date)), endOfDay(endOfMonth(date))] } },
       include: [
-        { model: Account, as: 'account', attributes: ['id', 'name'], where: { user_id: userId }, required: false },
-        { model: CreditCard, as: 'creditCard', attributes: ['id', 'name'], where: { userId }, required: false },
+        {
+          model: Account,
+          as: 'account',
+          attributes: ['id', 'name'],
+          where: { user_id: userId },
+          required: false,
+          include: {
+            model: Bank,
+            as: 'bank',
+            attributes: ['id', 'name', 'iconName', 'iconUrl'],
+          },
+        },
+        {
+          model: CreditCard,
+          as: 'creditCard',
+          attributes: ['id', 'name'],
+          where: { userId },
+          required: false,
+          include: {
+            model: Bank,
+            as: 'bank',
+            attributes: ['id', 'name', 'iconName', 'iconUrl'],
+          },
+        },
       ],
       order: [['recordDate', 'ASC']],
     });
 
-    return records;
+    const grouped = this.groupBy(records, record => record.recordDay);
+
+    const keys = [...grouped.keys()];
+
+    const formattedRecords = [];
+
+    keys.forEach(key => {
+      formattedRecords.push({
+        day: key,
+        records: grouped.get(key),
+      });
+    });
+
+    return formattedRecords;
+  }
+
+  groupBy(list, keyGetter) {
+    const map = new Map();
+    list.forEach(record => {
+      const key = keyGetter(record);
+      const collection = map.get(key);
+
+      if (!collection) {
+        map.set(key, [record]);
+      } else {
+        collection.push(record);
+      }
+    });
+
+    return map;
   }
 }
 
